1d628135f2b664edabe06d91130b58b178170104
yangyang8599 <yangyang8599@163.com>
2023-05-06 16:53:22 +0800

    This is NewVersion v1.1.63 Created after Release of 1.1.62

--- storm-boot-framework/src/main/java/cn/miniants/framework/advice/MiniFeignDecoder.java
+++ storm-boot-framework/src/main/java/cn/miniants/framework/advice/MiniFeignDecoder.java
@@ -2,10 +2,12 @@ package cn.miniants.framework.advice;
 
 import cn.hutool.core.util.ObjectUtil;
 import cn.miniants.framework.api.ApiResult;
+import cn.miniants.framework.exception.MiniFeignException;
 import cn.miniants.toolkit.JSONUtil;
 import feign.Response;
 import feign.codec.Decoder;
 import feign.codec.ErrorDecoder;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
 import org.springframework.cloud.openfeign.support.SpringDecoder;
@@ -21,6 +23,7 @@ import java.util.stream.Collectors;
 
 import static cn.miniants.framework.constant.StormwindConstant.MiniConstants.HTTP_HEAD_MINI_API;
 
+@Slf4j
 public class MiniFeignDecoder implements Decoder {
 
     private final Decoder delegate;
@@ -34,17 +37,19 @@ public class MiniFeignDecoder implements Decoder {
         //如果是框架的api都会包装成ApiResult，所以这里需要判断一下
         Map<String, Collection<String>> headers = response.headers();
         if (ObjectUtil.isNotNull(response.headers().get(HTTP_HEAD_MINI_API)) && headers.get(HTTP_HEAD_MINI_API).stream().anyMatch(value -> value.contains("true"))
-                && ( headers.get("Content-Type").stream().anyMatch(value -> value.contains("text/plain") || value.contains("application/json")))
+                && (headers.get("Content-Type").stream().anyMatch(value -> value.contains("text/plain") || value.contains("application/json")))
         ) {
             Type wrappedType = new ParameterizedType() {
                 @Override
                 public Type[] getActualTypeArguments() {
                     return new Type[]{type};
                 }
+
                 @Override
                 public Type getRawType() {
                     return ApiResult.class;
                 }
+
                 @Override
                 public Type getOwnerType() {
                     return null;
@@ -54,6 +59,9 @@ public class MiniFeignDecoder implements Decoder {
                 String responseBody = reader.lines().collect(Collectors.joining("\n"));
                 ApiResult<?> apiResponse = JSONUtil.readValue(responseBody, wrappedType);
                 return null == apiResponse ? null : apiResponse.getData();
+            } catch (Exception e) {
+                log.error("解码FeignClient报文错误，请检查参数及其他配置.", e);
+                throw new MiniFeignException(503, "解码FeignClient报文错误，请检查参数及其他配置，错误：%s".formatted(e.getMessage()));
             }
         }
 
