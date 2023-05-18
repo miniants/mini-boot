d9e309c5e468a7ba1fce80a8e3162e92d67b8c0f
yangyang8599 <yangyang8599@163.com>
2023-05-18 13:18:54 +0800

    This is NewVersion v1.1.72 Created after Release of 1.1.71

--- storm-boot-toolkit/src/main/java/cn/miniants/toolkit/EnvLoader.java
+++ storm-boot-toolkit/src/main/java/cn/miniants/toolkit/EnvLoader.java
@@ -1,16 +1,19 @@
 package cn.miniants.toolkit;
 
 import cn.hutool.core.util.StrUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.support.PropertiesLoaderUtils;
 
 import java.io.IOException;
 import java.util.Properties;
 
+@Slf4j
 public class EnvLoader {
 
     public static void load() {
         String activeEnv = System.getProperty("ACTIVE_NAMESPACE");
+        log.info("=========>ACTIVE_NAMESPACE: {}", activeEnv);
         if (activeEnv == null || activeEnv.isEmpty()) {
             try {
                 Properties properties = PropertiesLoaderUtils.loadProperties(new ClassPathResource("dev.env"));
