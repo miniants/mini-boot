5ecf9911bebe9995caa5fdbbe14756842c62aef9
yangyang8599 <yangyang8599@163.com>
2023-05-30 17:36:34 +0800

    This is NewVersion v1.1.77 Created after Release of 1.1.76

--- storm-boot-toolkit/src/main/java/cn/miniants/toolkit/OrderedProperties.java
+++ storm-boot-toolkit/src/main/java/cn/miniants/toolkit/OrderedProperties.java
@@ -5,88 +5,105 @@ import org.springframework.core.io.ClassPathResource;
 
 import java.util.*;
 import java.io.*;
+import java.util.function.BiConsumer;
 
 
 public class OrderedProperties extends Properties {
-    private final LinkedHashMap<Object, Object> properties = new LinkedHashMap<>();
+    private final LinkedHashMap<Object, Object> linkedProperties = new LinkedHashMap<>();
 
     @Override
     public synchronized Object put(Object key, Object value) {
-        return properties.put(key, value);
+        return linkedProperties.put(key, value);
     }
 
     @Override
     public synchronized Object remove(Object key) {
-        return properties.remove(key);
+        return linkedProperties.remove(key);
     }
 
     @Override
     public synchronized void clear() {
-        properties.clear();
+        linkedProperties.clear();
+    }
+
+    @Override
+    public synchronized boolean contains(Object value) {
+        return linkedProperties.containsValue(value);
+    }
+
+    @Override
+    public synchronized boolean containsKey(Object key) {
+        return linkedProperties.containsKey(key);
+    }
+
+    @Override
+    public synchronized boolean containsValue(Object value) {
+        return linkedProperties.containsValue(value);
+    }
+
+    @Override
+    public synchronized Enumeration<Object> elements() {
+        return Collections.enumeration(linkedProperties.values());
+    }
+
+    @Override
+    public Set<Map.Entry<Object, Object>> entrySet() {
+        return linkedProperties.entrySet();
+    }
+
+    @Override
+    public synchronized void forEach(BiConsumer<? super Object, ? super Object> action) {
+        linkedProperties.forEach(action);
+    }
+
+    @Override
+    public synchronized Object get(Object key) {
+        return linkedProperties.get(key);
+    }
+
+    @Override
+    public synchronized boolean isEmpty() {
+        return linkedProperties.isEmpty();
+    }
+
+    @Override
+    public Enumeration<Object> keys() {
+        return Collections.enumeration(linkedProperties.keySet());
     }
 
     @Override
     public Set<Object> keySet() {
-        return properties.keySet();
+        return linkedProperties.keySet();
+    }
+
+    @Override
+    public synchronized Enumeration<Object> propertyNames() {
+        return Collections.enumeration(linkedProperties.keySet());
+    }
+
+    @Override
+    public synchronized Object setProperty(String key, String value) {
+        return linkedProperties.put(key, value);
+    }
+
+    @Override
+    public synchronized int size() {
+        return linkedProperties.size();
+    }
+
+    @Override
+    public synchronized Collection<Object> values() {
+        return linkedProperties.values();
     }
 
     @Override
     public Set<String> stringPropertyNames() {
         Set<String> set = new HashSet<>();
-        for (Object key : this.properties.keySet()) {
+        for (Object key : linkedProperties.keySet()) {
             set.add((String) key);
         }
         return set;
     }
-
-    @Override
-    public synchronized boolean contains(Object value) {
-        return properties.containsValue(value);
-    }
-
-    @Override
-    public boolean containsValue(Object value) {
-        return properties.containsValue(value);
-    }
-
-    @Override
-    public synchronized boolean containsKey(Object key) {
-        return properties.containsKey(key);
-    }
-
-    @Override
-    public synchronized Object get(Object key) {
-        return properties.get(key);
-    }
-
-    @Override
-    public synchronized int size() {
-        return properties.size();
-    }
-
-    @Override
-    public synchronized boolean isEmpty() {
-        return properties.isEmpty();
-    }
-
-    @Override
-    public synchronized Enumeration<Object> keys() {
-        Iterator<Object> it = properties.keySet().iterator();
-        return new Enumeration<Object>() {
-            public boolean hasMoreElements() {
-                return it.hasNext();
-            }
-            public Object nextElement() {
-                return it.next();
-            }
-        };
-    }
-
-    @Override
-    public Set<Map.Entry<Object, Object>> entrySet() {
-        return properties.entrySet();
-    }
-
 
     private static final String XML_FILE_EXTENSION = ".xml";
     private static final boolean shouldIgnoreXml = SpringProperties.getFlag("spring.xml.ignore");
