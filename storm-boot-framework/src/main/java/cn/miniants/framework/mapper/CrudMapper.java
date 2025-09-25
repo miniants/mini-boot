cbdf3ad3fd0c26e9f19e38d04894e5d73e50aea3
yangyang8599 <yangyang8599@163.com>
2025-09-25 12:09:31 +0800

    只有逻辑删除的表才会添加这个方法。这个控制逻辑在@MiniSqlInjector中。

--- storm-boot-framework/src/main/java/cn/miniants/framework/mapper/CrudMapper.java
+++ storm-boot-framework/src/main/java/cn/miniants/framework/mapper/CrudMapper.java
@@ -11,5 +11,10 @@ public interface CrudMapper<T> extends BaseMapper<T> {
 
     Integer insertBatch(@Param("list") Collection<T> t);
 
+    /**
+     * 只有逻辑删除的表才会添加这个方法。这个控制逻辑在@MiniSqlInjector中。
+     * @param ids
+     * @return
+     */
     int recoverByIds(@Param("ids") Collection<?> ids);
 }
